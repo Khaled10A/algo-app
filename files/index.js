3df5a045ea70a248520db37a5c5d1148ba4e49bc
Khaled Alnajjar <khaledalnajjar079@gmail.com>
2026-05-28 13:41:33 +0300

    clean up unnecessary files

--- files/index.js
+++
@@ -1,5 +0,0 @@
-export { insertionSort, insertionSortSteps } from './insertionSort';
-export { mergeSort } from './mergeSort';
-export { quickSort } from './quickSort';
-export { selectionSort, selectionSortSteps } from './selectionSort';
-export { bubbleSort, bubbleSortSteps } from './bubbleSort';
--- files/mnt/user-data/outputs/algo-app/src/algorithms/searching/index.js
+++
@@ -1,3 +0,0 @@
-export { bruteForceSearch } from './bruteForce';
-export { horspoolSearch } from './horspool';
-export { kmpSearch } from './kmp';
